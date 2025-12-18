ca3b81e7ddd072a1e2b688be39a8de8bfe735081
Adrian Maslerz <adrianmaslerz@interia.eu>
2025-12-18 01:04:07 +0100

    readme adjustments

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Smart Hotel API')
+    .setDescription('API documentation for Smart Hotel management system')
+    .setVersion('1.0')
+    .addTag('Hotel')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+  console.log(
+    `Application running on http://localhost:${process.env.PORT ?? 3000}`,
+  );
 }
 bootstrap();
